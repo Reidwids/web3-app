4f0a55049a739915da51681ee2d0ce20343a3cf4
reidwids <reidwids@gmail.com>
2024-02-13 20:55:27 -0500

    Add react router dom

--- src/pages/Base.tsx
+++ src/pages/Base.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./Navbar";
 import Lander from "./lander/Lander";
 
@@ -6,7 +7,15 @@ export default function Base() {
 	return (
 		<div className="w-full min-h-screen flex flex-col items-center">
 			<Navbar />
-			<Lander />
+			<div className="min-w-full min-h-[calc(100vh-72px)] flex justify-center ">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Lander />} />
+						{/* <Route path="/Details" element={<Lander />} />
+						<Route path="/Transfer" element={<Lander />} /> */}
+					</Routes>
+				</BrowserRouter>
+			</div>
 		</div>
 	);
 }
