c30d40bbb864ab42dd0b7d245b0efe67a9810818
reidwids <reidwids@gmail.com>
2024-02-15 09:32:35 -0500

    fix toast formatting add animation to token input

--- src/pages/transfer/Transfer.tsx
+++ src/pages/transfer/Transfer.tsx
@@ -2,19 +2,20 @@ import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import abi from "../../utils/erc20.json";
 import { toast } from "react-toastify";
+import { motion } from "framer-motion";
+import { defaultToastOptions } from "../../context/TransactionContext";
 
 export default function Transfer() {
 	const [amount, setAmount] = useState<string>("");
 	const [toAddress, setToAddress] = useState<string>("");
 	const [tokenAddress, setTokenAddress] = useState<string>("");
 	const [tokenType, setTokenType] = useState<"eth" | "token">("eth");
-	const [transactionData, setTransactionData] = useState<any>({});
 
 	const sendTransaction = async (e: React.FormEvent) => {
 		e.preventDefault();
 
 		if (!toAddress || !amount) {
-			toast.error("Please fill in all fields");
+			toast.error("Please fill in all fields", defaultToastOptions);
 			return;
 		}
 		const provider = new ethers.BrowserProvider(window.ethereum!);
@@ -25,25 +26,23 @@ export default function Transfer() {
 					to: toAddress,
 					value: ethers.parseEther(amount),
 				};
-				const txResponse = await signer.sendTransaction(tx);
-				setTransactionData(txResponse);
+				await signer.sendTransaction(tx);
 			} catch (e) {
 				console.log("Error completing transaction", e);
-				toast.error("Error completing transaction");
+				toast.error("Error completing transaction", defaultToastOptions);
 			}
 		} else {
 			if (!tokenAddress) {
-				toast.error("Please fill in all fields");
+				toast.error("Please fill in all fields", defaultToastOptions);
 				return;
 			}
 
 			try {
 				const contract = new ethers.Contract(tokenAddress, abi, signer);
-				const txResponse = await contract.transfer(toAddress, ethers.parseUnits(amount, "ether"));
-				setTransactionData(txResponse);
+				await contract.transfer(toAddress, ethers.parseUnits(amount, "ether"));
 			} catch (e) {
 				console.log("Error completing transaction", e);
-				toast.error("Error completing transaction");
+				toast.error("Error completing transaction", defaultToastOptions);
 			}
 		}
 	};
@@ -55,38 +54,29 @@ export default function Transfer() {
 	return (
 		<div className="w-full flex items-center justify-center bg-neutral-800 text-white">
 			<div className="w-full flex flex-col items-center justify-center max-w-4xl min-h-80 shadow-2xl rounded-xl">
-				<div className="flex justify-center w-full text-3xl">
-					<div
-						className="bg-blue-950 w-1/2 p-2 py-2 text-center cursor-pointer"
-						style={{
+				<div className="flex justify-center w-full text-lg rounded-xl overflow-hidden max-w-60">
+					<motion.div
+						animate={{
 							backgroundColor: tokenType === "eth" ? "rgb(30 64 175)" : "rgb(23 37 84)",
+							transition: { duration: 0.3 },
 						}}
+						className="bg-blue-950 w-1/2 p-2 py-2 text-center cursor-pointer"
 						onClick={() => setTokenType("eth")}
 					>
-						Eth
-					</div>
-					<div
+						ETH
+					</motion.div>
+					<motion.div
 						className="bg-blue-950 w-1/2 p-2 py-2 text-center cursor-pointer"
-						style={{
-							backgroundColor: tokenType === "token" ? "rgb(30 64 175)" : "rgb(23 37 84)",
+						animate={{
+							backgroundColor: tokenType !== "eth" ? "rgb(30 64 175)" : "rgb(23 37 84)",
+							transition: { duration: 0.3 },
 						}}
 						onClick={() => setTokenType("token")}
 					>
-						Token
-					</div>
+						TOKEN
+					</motion.div>
 				</div>
 				<form className="flex flex-col items-start my-10">
-					{tokenType === "token" && (
-						<>
-							<label>Token Address:</label>
-							<input
-								className="w-80 p-2 m-2 text-black rounded-md"
-								type="text"
-								placeholder="0x"
-								onChange={(e) => setTokenAddress(e.target.value)}
-							/>
-						</>
-					)}
 					<label>To Address:</label>
 					<input
 						className="w-80 p-2 m-2 text-black rounded-md"
@@ -101,6 +91,24 @@ export default function Transfer() {
 						placeholder="0.0"
 						onChange={(e) => setAmount(e.target.value)}
 					/>
+					{tokenType === "token" ? (
+						<motion.div
+							className="flex flex-col"
+							initial={{ opacity: 0, x: -20 }}
+							animate={{ opacity: 1, x: 0 }}
+							transition={{ duration: 0.5 }}
+						>
+							<label>Token Address:</label>
+							<input
+								className="w-80 p-2 m-2 text-black rounded-md"
+								type="text"
+								placeholder="0x"
+								onChange={(e) => setTokenAddress(e.target.value)}
+							/>
+						</motion.div>
+					) : (
+						<div className="h-20"></div>
+					)}
 					<button className="w-80 p-2 m-2 bg-blue-950 hover:bg-blue-800 transition-all rounded-md" onClick={sendTransaction}>
 						Send
 					</button>
